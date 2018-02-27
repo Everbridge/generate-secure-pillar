2d62218e2a7b8d6274d12a6641e36570e60aa4da
Ed Silva <ed.silva@everbridge.com>
2018-02-27 11:06:24 -0800

    new master build: 1.0.134

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 		logger.Level = logrus.DebugLevel
 	}
 	app := cli.NewApp()
-	app.Version = "1.0.131"
+	app.Version = "1.0.134"
 	app.Authors = []cli.Author{
 		cli.Author{
 			Name:  "Ed Silva",
