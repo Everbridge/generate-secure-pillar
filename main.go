3a9f84bc5c35c92640c37dff54095b47106624f3
Ed Silva <ed.silva@everbridge.com>
2018-03-05 14:44:08 -0800

    update docs

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 		logger.Level = logrus.DebugLevel
 	}
 	app := cli.NewApp()
-	app.Version = "1.0.164"
+	app.Version = "1.0.158"
 	app.Authors = []cli.Author{
 		cli.Author{
 			Name:  "Ed Silva",
@@ -75,6 +75,8 @@ $ generate-secure-pillar -k "Salt Master" update --name secret_name --value secr
 
 # encrypt all plain text values in a file
 $ generate-secure-pillar -k "Salt Master" encrypt all --file us1.sls --outfile us1.sls
+# or use --update flag
+$ generate-secure-pillar -k "Salt Master" encrypt all --file us1.sls --update
 
 # encrypt all plain text values in a file under the element 'secret_stuff'
 $ generate-secure-pillar -k "Salt Master" --element secret_stuff encrypt all --file us1.sls --outfile us1.sls
