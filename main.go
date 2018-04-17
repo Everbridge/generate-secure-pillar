4a3e9ca504b78c1feed3d9f15f5f4fca0bfbaafb
Ed Silva <ed.silva@everbridge.com>
2018-04-17 16:43:27 -0700

    new master build: 1.0.294

--- main.go
+++ main.go
@@ -375,7 +375,7 @@ var appCommands = []cli.Command{
 
 func main() {
 	app := cli.NewApp()
-	app.Version = "1.0.293"
+	app.Version = "1.0.294"
 	app.Authors = []cli.Author{
 		cli.Author{
 			Name:  "Ed Silva",
