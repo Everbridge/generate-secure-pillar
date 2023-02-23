7513a30b77ce7757f47265fddc181922c0823b04
Ed Silva <ed@mini-me.local>
2023-02-23 06:51:05 -0800

    added package comments

--- sls/sls.go
+++ sls/sls.go
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+// The SLS package handles parsing of Salt Pillar files
 package sls
 
 import (
--- utils/utils.go
+++ utils/utils.go
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+// The Utils package is for general utility functions
 package utils
 
 import (
