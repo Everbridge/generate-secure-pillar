e9a668ddc103bca4adb00eec12e10a592e499177
Ed Silva <ed.silva@everbridge.com>
2023-03-10 08:39:15 -0800

    new main build: 1.0.604

--- cmd/root.go
+++ cmd/root.go
@@ -93,7 +93,7 @@ $ generate-secure-pillar keys recurse -d /path/to/pillar/secure/stuff
 # show the PGP Key ID used for an element at a path in a file
 $ generate-secure-pillar keys path --path "some:yaml:path" --file new.sls
 `,
-	Version: "1.0.602",
+	Version: "1.0.604",
 }
 
 const all = "all"
